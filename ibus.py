9b5bb381a14ad49b80775990d32aac8155cd91cb
Fusekai <inouchi@outlook.com>
2019-01-18 14:34:55 -0300

    fix space

--- ibus.py
+++ ibus.py
@@ -123,7 +123,7 @@ class UniEmojiIBusEngine(IBus.Engine):
                     self.commit_candidate()
                     return True
                 return False
-             elif keyval == IBus.Page_Up or keyval == IBus.KP_Page_Up or keyval == IBus.Left or keyval == IBus.KP_Left:
+            elif keyval == IBus.Page_Up or keyval == IBus.KP_Page_Up or keyval == IBus.Left or keyval == IBus.KP_Left:
                 self.page_up()
                 return True
             elif keyval == IBus.Page_Down or keyval == IBus.KP_Page_Down or keyval == IBus.Right or keyval == IBus.KP_Right:
