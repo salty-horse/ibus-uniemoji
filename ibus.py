0ff4929f95e26d0d14265ab23d53b72aa8982026
Fusekai <inouchi@outlook.com>
2019-01-21 13:53:29 -0300

    make the keyval checks less lengthy

--- ibus.py
+++ ibus.py
@@ -123,19 +123,17 @@ class UniEmojiIBusEngine(IBus.Engine):
                     self.commit_candidate()
                     return True
                 return False
-            elif keyval == IBus.Page_Up or keyval == IBus.KP_Page_Up or keyval == IBus.Left or keyval == IBus.KP_Left:
+            elif keyval in (IBus.Page_Up, IBus.KP_Page_Up, IBus.Left, IBus.KP_Left):
                 self.page_up()
                 return True
-            elif keyval == IBus.Page_Down or keyval == IBus.KP_Page_Down or keyval == IBus.Right or keyval == IBus.KP_Right:
+            elif keyval in (IBus.Page_Down, IBus.KP_Page_Down, IBus.Right, IBus.KP_Right):
                 self.page_down()
                 return True
-            elif keyval == IBus.Up or keyval == IBus.KP_Up:
+            elif keyval in (IBus.Up, IBus.KP_Up):
                 self.cursor_up()
                 return True
-            elif keyval == IBus.Down or keyval == IBus.KP_Down:
+            elif keyval in (IBus.Down, IBus.KP_Down):
                 self.cursor_down()
-                return True
-            elif keyval == IBus.Left or keyval == IBus.Right:
                 return True
 
         if keyval == IBus.space and len(self.preedit_string) == 0:
