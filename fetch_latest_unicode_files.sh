b76b8805eb290d002c07c822dc55f16d108d4cc7
Ori Avtalion <ori@avtalion.name>
2025-09-10 09:22:03 +0300

    Update to Unicode & Emoji 17.0

---
+++ fetch_latest_unicode_files.sh
@@ -0,0 +1,4 @@
+curl \
+	-o unicode/UnicodeData.txt https://unicode.org/Public/UNIDATA/UnicodeData.txt \
+    -o unicode/emoji-sequences.txt https://www.unicode.org/Public/emoji/latest/emoji-sequences.txt \
+    -o unicode/emoji-zwj-sequences.txt https://www.unicode.org/Public/emoji/latest/emoji-zwj-sequences.txt
